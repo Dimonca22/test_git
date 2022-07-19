b61e5443eb2cce110775d6bd9fbdc9994673d3c3
dimonca <sologub1995@mail.ru>
2022-07-19 16:24:14 +0600

    Добавлена модуль strings.py, добавлена функция mul

--- test/test.py
+++ test/test.py
@@ -1,12 +1,13 @@
 def plus (a,b):
     return a+b
 
-
-text = "NEW TEXT"
-
 def minus(a,b):
     return a-b
+
+def mul(a,b):
+    return a*b
 
 if __name__ == '__main__':
     print(plus(5,6))
     print(minus(10,3))
+    print(mul(3,3))
